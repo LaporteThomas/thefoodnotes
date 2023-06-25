23e03ecaf31519d0c2c5da3498106f0ba92ec73a
LaporteThomas <laporte.thomas@outlook.com>
2023-06-25 21:08:55 +0200

    Need to update generate_recipe with ceviche recipe

--- js/js_recipe.js
+++ js/js_recipe.js
@@ -7,15 +7,24 @@ var bottomBlock = document.getElementById('bottom');
 var windowHeight = window.innerHeight;
 var headerBlockHeight = headerBlock.offsetHeight;
 var bottomBlockHeight = bottomBlock.offsetHeight;
-var classBlockHeight = windowHeight - headerBlockHeight - bottomBlockHeight;
+// var classBlockHeight = windowHeight - headerBlockHeight - bottomBlockHeight - classBlock.style.paddingTop - classBlock.style.paddingBottom;
+var classBlockHeight = windowHeight - headerBlockHeight - bottomBlockHeight - 60 - 60;
 
 // Set the height of the class block
 classBlock.style.height = classBlockHeight + 'px';
 
-var sectionInfoBlock = document.getElementById('sectionInfo')
-var ingredientRecipeBlock = document.getElementById('sectionIngredientRecipe')
+var sectionInfoBlock = document.getElementById('sectioninfo')
+var ingredientRecipeBlock = document.getElementById('sectioningredientrecipe')
 
 var sectionInfoBlockHeight = sectionInfoBlock.offsetHeight;
 var ingredientRecipeBlockHeight = classBlockHeight - sectionInfoBlockHeight;
 
-ingredientRecipeBlock.style.height = ingredientRecipeBlockHeight + 'px;'
+ingredientRecipeBlock.style.height = ingredientRecipeBlockHeight + 'px';
+
+var ingredienttitleinnerboxBlock = document.getElementById('ingredienttitleinnerbox')
+var ingredientlistinnerboxBlock = document.getElementById('ingredientlistinnerbox')
+
+var ingredienttitleinnerboxBlockHeight = ingredienttitleinnerboxBlock.offsetHeight;
+var ingredientlistinnerboxBlockHeight = ingredientRecipeBlockHeight - ingredienttitleinnerboxBlockHeight - 40;
+
+ingredientlistinnerboxBlock.style.height = ingredientlistinnerboxBlockHeight + 'px';
